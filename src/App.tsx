721163bd75f4ad2c2902f4baf2860f25a949698a
Ilyass Rachouady <ilyasstube80@gmail.com>
2025-11-27 16:55:37 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 import { Toaster } from '@/components/ui/sonner';
 import DashboardLayout from './layouts/DashboardLayout';
 import DashboardHome from './pages/dashboard/DashboardHome';
@@ -7,19 +8,21 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Show dashboard home on / and /dashboard */}
-        <Route path="/" element={<DashboardLayout />}>
-          <Route index element={<DashboardHome />} />
-        </Route>
-        <Route path="/dashboard" element={<DashboardLayout />}>
-          <Route index element={<DashboardHome />} />
-        </Route>
+      <AuthProvider>
+        <Routes>
+          {/* Show dashboard home on / and /dashboard */}
+          <Route path="/" element={<DashboardLayout />}>
+            <Route index element={<DashboardHome />} />
+          </Route>
+          <Route path="/dashboard" element={<DashboardLayout />}>
+            <Route index element={<DashboardHome />} />
+          </Route>
 
-        {/* Redirect all other routes to dashboard home */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-      <Toaster />
+          {/* Redirect all other routes to dashboard home */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+        <Toaster />
+      </AuthProvider>
     </BrowserRouter>
   );
 }
